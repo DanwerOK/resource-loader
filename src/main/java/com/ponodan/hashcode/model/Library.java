29549a0c41347103111993a984214fd5de43df1c
Oleksandr Khomenko <khomenko.dp@gmail.com>
2020-02-20 21:40:38 +0200

    calc potential library score

--- src/main/java/com/ponodan/hashcode/model/Library.java
+++ src/main/java/com/ponodan/hashcode/model/Library.java
@@ -1,5 +1,7 @@
 package com.ponodan.hashcode.model;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Library {
@@ -8,4 +10,25 @@ public class Library {
     public int signupDelay;
     public int shipPerDay;
     public List<Book> books;
+
+    private List<Book> sortedBooksByScore;
+
+    public int potentialScore(int deadline, Book... exludedBooks) {
+        if (sortedBooksByScore == null) {
+            ArrayList<Book> books1 = new ArrayList<>(this.books);
+            books1.sort(Comparator.comparing(book -> (book.score)));
+            this.sortedBooksByScore = books1;
+        }
+
+        int potentialBookAmount = (deadline - shipPerDay) * shipPerDay;
+
+        int potentialLibraryScore = 0;
+        for (int i = 0; i < potentialBookAmount; i++) {
+            Book book = sortedBooksByScore.get(i);
+            if (book != null) {
+                potentialLibraryScore += book.score;
+            }
+        }
+        return potentialLibraryScore;
+    }
 }
