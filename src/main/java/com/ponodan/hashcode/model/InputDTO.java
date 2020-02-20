2f14a6c1653af22697ab75a8d16e86c36bfdb0a3
Oleksandr Khomenko <khomenko.dp@gmail.com>
2020-02-20 21:48:44 +0200

    getters InputDTO

--- src/main/java/com/ponodan/hashcode/model/InputDTO.java
+++ src/main/java/com/ponodan/hashcode/model/InputDTO.java
@@ -9,4 +9,24 @@ public class InputDTO {
     public int scanDaysAmount;
     public List<Library> libraries;
     public HashMap<Integer, Book> books;
+
+    public int getBooksAmount() {
+        return booksAmount;
+    }
+
+    public int getLibrariesAmount() {
+        return librariesAmount;
+    }
+
+    public int getScanDaysAmount() {
+        return scanDaysAmount;
+    }
+
+    public List<Library> getLibraries() {
+        return libraries;
+    }
+
+    public HashMap<Integer, Book> getBooks() {
+        return books;
+    }
 }
