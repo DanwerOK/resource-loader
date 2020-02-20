d92ccf2cd52844b55d255106f57b0dd2908fc724
Oleksandr Khomenko <khomenko.dp@gmail.com>
2020-02-20 21:48:11 +0200

    getters library

--- src/main/java/com/ponodan/hashcode/model/Library.java
+++ src/main/java/com/ponodan/hashcode/model/Library.java
@@ -13,22 +13,45 @@ public class Library {
 
     private List<Book> sortedBooksByScore;
 
-    public int potentialScore(int deadline, Book... exludedBooks) {
+    public List<Book> getSortedBooksByScore() {
         if (sortedBooksByScore == null) {
             ArrayList<Book> books1 = new ArrayList<>(this.books);
             books1.sort(Comparator.comparing(book -> (book.score)));
             this.sortedBooksByScore = books1;
         }
+        return sortedBooksByScore;
+    }
 
+    public int potentialScore(int deadline, Book... exludedBooks) {
         int potentialBookAmount = (deadline - shipPerDay) * shipPerDay;
 
         int potentialLibraryScore = 0;
         for (int i = 0; i < potentialBookAmount; i++) {
-            Book book = sortedBooksByScore.get(i);
+            Book book = getSortedBooksByScore().get(i);
             if (book != null) {
                 potentialLibraryScore += book.score;
             }
         }
         return potentialLibraryScore;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBooksAmount() {
+        return booksAmount;
+    }
+
+    public int getSignupDelay() {
+        return signupDelay;
+    }
+
+    public int getShipPerDay() {
+        return shipPerDay;
+    }
+
+    public List<Book> getBooks() {
+        return books;
     }
 }
